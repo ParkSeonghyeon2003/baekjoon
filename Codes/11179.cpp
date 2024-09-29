96ed4c94215c24e12f078fd7a85e19a256b83747
박성현 <shpark8267@gmail.com>
2024-09-29 23:57:16 +0900

    Solve 11179

--- Codes/11179.cpp
+++ Codes/11179.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main(void)
 {
-	int N;
+	int N, result = 0;
 	vector<int> bin;
 
 	cin >> N;
@@ -12,9 +13,9 @@ int main(void)
 	for (int i = N; i > 0; i /= 2) {
 		bin.push_back(i % 2);
 	}
-
 	for (int i = 0; i < bin.size(); i++) {
-		cout << bin[i];
+		result += bin[i] * pow(2, bin.size() - i-1);
 	}
+	cout << result << '\n';
 	return 0;
 }
